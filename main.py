2f6906238c91d9d62c4ff7602fd0ca0e59878d87
Catalin Gama <catalin.gama@ngi.systems>
2022-01-13 09:22:44 +0200

    added real time verification

--- main.py
+++ main.py
@@ -353,9 +353,61 @@ def test_run():
     show_test_img(test_input[0], test_val[0])
 
 
+def verify(model, detection_threshold, verification_threshold):
+    # Build results array
+    results = []
+    # for image in os.listdir(os.path.join('application_data', 'verification_images')):
+
+
+
+    input_img = preprocess(os.path.join('img_verification', 'input_image', 'input_image.jpg'))
+    validation_img = preprocess(os.path.join('img_verification', 'validation_image', 'validation_image.jpg'))
+
+    # Make Predictions
+    result = model.predict(list(np.expand_dims([input_img, validation_img], axis=1)))
+    results.append(result)
+
+
+
+
+
+    # Detection Threshold: Metric above which a prediciton is considered positive
+    detection = np.sum(np.array(results) > detection_threshold)
+
+    # Verification Threshold: Proportion of positive predictions / total positive samples
+    # verification = detection / len(os.listdir(os.path.join('application_data', 'validation_images')))
+    verification = detection / len(results)
+    verified = verification > verification_threshold
+
+    return results, verified
+
+def rt_run():
+    cap = cv2.VideoCapture(0)
+    siamese_model = load_model(MODEL_PATH)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame = frame[120:120 + 250, 200:200 + 250, :]
+
+        cv2.imshow('Verification', frame)
+
+
+        if cv2.waitKey(10) & 0xFF == ord('v'):
+            # Save input image to application_data/input_image folder
+            cv2.imwrite(os.path.join('img_verification', 'input_image', 'input_image.jpg'), frame)
+            # Run verification
+            results, verified = verify(siamese_model, 0.7, 0.7)
+            print(verified)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def run():
     # train_run()
-    test_run()
+    # test_run()
+    rt_run()
 
 
 if __name__ == "__main__":
